09a5e9d7a4135cc4baaf22262649338b9c3bd46b
Emmanuel Enchill <mzigah4@gmail.com>
2024-06-07 05:12:38 +0000

    Task 0 - a script that queries the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Get number of subscribers
+
+    Description:
+    This function obtains the number of subscribers of a given
+    subreddit
+
+    Returns
+    The total number or 0 if an invalid subreddit is given
+    """
+    
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+
+    return results.get("subscribers")
